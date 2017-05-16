cc2ebec876500a2c8bb80c179c38ad3e131a65a5
Petr Škoda <skodapetr@gmail.com>
2017-05-16 10:53:19 +0200

    Add connection to catalog

---
+++ js/script.js
@@ -0,0 +1,38 @@
+(function initialize() {
+
+    // Must NOT end with.
+    const CATALOG_URL = "https://novy.nkod.opendata.cz";
+
+    function initializeFormAction() {
+        document.getElementById("searchForm").action = CATALOG_URL;
+    }
+
+    function readNumberOfDatasets() {
+        const url = CATALOG_URL + "/api/v1/solr/info";
+        fetch(url).then((response) => {
+            return response.json();
+        }).then((body) => {
+            const datasetCount = numberToStringAddSpaces(body.data.numberOfDatasets);
+            document.getElementById("datasetCountLabel").textContent = datasetCount;
+            document.getElementById("datasetCount").style.visibility = "visible";
+        });
+    }
+
+    function numberToStringAddSpaces(value) {
+        let valueAsString = value.toString();
+        let output = "";
+        for (let index = 0; index < valueAsString.length; ++index) {
+            if ((valueAsString.length - index) % 3 === 0) {
+                output += " ";
+            }
+            output += valueAsString[index];
+        }
+        return output;
+        return value;
+    }
+
+    initializeFormAction();
+    readNumberOfDatasets();
+
+})();
+

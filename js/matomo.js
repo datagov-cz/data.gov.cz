7943017a383845434c8d93ee35f2f1754daae6b2
Jakub Klímek <jakub@jakubklimek.com>
2023-09-27 13:52:13 +0200

    Lepší umístění Matomo
    Signed-off-by: Jakub Klímek <jakub@jakubklimek.com>

--- js/matomo.js
+++ js/matomo.js
@@ -1,9 +1,10 @@
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["disableCookies"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-var u='https://matomoas-stage.westeurope.cloudapp.azure.com/';
+var u="https://matomoas-stage.westeurope.cloudapp.azure.com/";
 _paq.push(['setTrackerUrl', u+'matomo.php']);
 _paq.push(['setSiteId', '15']);
 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
